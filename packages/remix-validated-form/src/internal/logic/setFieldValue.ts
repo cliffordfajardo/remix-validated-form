8ee7ce7518f333d3bd39d11d5fe0efb28f4615f5
Aaron Pettengill <pettengill.aaron@gmail.com>
2022-02-08 23:05:12 -0500

    feat: add setFieldValue helper

---
+++ packages/remix-validated-form/src/internal/logic/setFieldValue.ts
@@ -0,0 +1,62 @@
+import invariant from "tiny-invariant";
+import { getCheckboxChecked } from "./getCheckboxChecked";
+import { getRadioChecked } from "./getRadioChecked";
+
+const setElementValue = (element: Node, value: unknown, name: string) => {
+  if (element instanceof HTMLSelectElement && element.multiple) {
+    invariant(
+      Array.isArray(value),
+      "Must specify an array to set the value for a multi-select"
+    );
+    for (const option of element.options) {
+      option.selected = value.includes(option.value);
+    }
+    return;
+  }
+
+  if (element instanceof HTMLInputElement && element.type === "checkbox") {
+    const newChecked = getCheckboxChecked(element.value, value);
+    invariant(
+      newChecked !== undefined,
+      `Unable to determine if checkbox should be checked. Provided value was ${value} for checkbox ${name}.`
+    );
+    element.checked = newChecked;
+    return;
+  }
+
+  if (element instanceof HTMLInputElement && element.type === "radio") {
+    const newChecked = getRadioChecked(element.value, value);
+    invariant(
+      newChecked !== undefined,
+      `Unable to determine if radio should be checked. Provided value was ${value} for radio ${name}.`
+    );
+    element.checked = newChecked;
+    return;
+  }
+
+  invariant(
+    typeof value === "string",
+    `Invalid value for field "${name}" which is an ${
+      element.constructor.name
+    }. Expected string but received ${typeof value}`
+  );
+  const input = element as HTMLInputElement;
+  input.value = value;
+};
+
+export const setFieldValue = (
+  formElement: HTMLFormElement,
+  name: string,
+  value: unknown
+) => {
+  const controlElement = formElement.elements.namedItem(name);
+  if (!controlElement) return;
+
+  if (controlElement instanceof RadioNodeList) {
+    for (const element of controlElement) {
+      setElementValue(element, value, name);
+    }
+  } else {
+    setElementValue(controlElement, value, name);
+  }
+};
